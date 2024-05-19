44369cd6cccdcbae83b7b01a8d77c4a0b8357407
jtmeyer98 <jtmeyer@uc.cl>
2024-05-18 20:03:27 -0400

    feat: crear página home con links a Admin y Api

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
-    path('', include('prevcad.urls'))
+    path('api/', include('prevcad.urls')),
 ]
