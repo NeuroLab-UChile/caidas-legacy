0fed37141a5e7a1f1c0e1e2275a6adfa694065e7
Elisa <elisahonoratolira@uc.cl>
2025-01-08 19:48:54 -0300

    collect static

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -188,11 +188,8 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    
-    os.path.join(BASE_DIR, 'prevcad', 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'prevcad', 'static')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
