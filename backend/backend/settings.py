75b40ca2b74f5c218893aa08273634db17e6ef80
Elisa <elisahonoratolira@uc.cl>
2025-02-27 10:55:18 -0300

    apk prebuild

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -61,11 +61,9 @@ CORS_ALLOW_METHODS = [
     "POST",
     "PUT",
 ]
-# Asegurarse que DEBUG está activado
-DEBUG = True
+
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_HEADERS = [
     "accept",
