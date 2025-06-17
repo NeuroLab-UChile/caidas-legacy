0654424ca3e8daa3bad299fb5c507f414ea40e64
Juano Venegas <jevenegasa@gmail.com>
2025-06-16 23:07:52 -0400

    API tests
    WIth UseBruno API tester.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from .views import home
@@ -20,21 +21,27 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 from rest_framework_simplejwt.views import (
-  TokenObtainPairView,
-  TokenRefreshView,
-  TokenVerifyView
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
 )
 
 from prevcad.views.admin_views import update_training_form, delete_training_node
 
-urlpatterns = [
-  path('', home, name='home'),
-  path('admin/', admin.site.urls),
-  path('api/', include('prevcad.urls')),
-  path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-  path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-  path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-  path('api/admin/categorytemplate/<int:template_id>/delete_node/<str:node_id>/', 
-       delete_training_node, 
-       name='delete_training_node'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = (
+    [
+        path("", home, name="home"),
+        path("admin/", admin.site.urls),
+        path("api/", include("prevcad.urls")),
+        path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+        path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+        path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+        path(
+            "api/admin/categorytemplate/<int:template_id>/delete_node/<str:node_id>/",
+            delete_training_node,
+            name="delete_training_node",
+        ),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
--- backend/prevcad/urls.py
+++ backend/prevcad/urls.py
@@ -4,7 +4,15 @@ from django.urls import path, include
 
 from prevcad.views.profiles import getProfile, uploadProfileImage, deleteProfileImage
 from prevcad.views.text_recomendations import TextRecomendationsView
-from prevcad.views.health_categories import HealthCategoryListView, save_evaluation_responses, create_health_category, update_health_category, update_recommendation, save_professional_evaluation, clear_evaluation
+from prevcad.views.health_categories import (
+    HealthCategoryListView,
+    save_evaluation_responses,
+    create_health_category,
+    update_health_category,
+    update_recommendation,
+    save_professional_evaluation,
+    clear_evaluation,
+)
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import admin_views
@@ -16,46 +24,92 @@ from .views import health_categories
 
 router = routers.DefaultRouter(trailing_slash=False)
 
-router.register('prevcad/text_recommendations/', TextRecomendationsView, basename='text_recommendations')
-router.register('prevcad/text_recommendations/<int:pk>/register_click', TextRecomendationsView, basename='recommendation-register-click')
+router.register(
+    "prevcad/text_recommendations/",
+    TextRecomendationsView,
+    basename="text_recommendations",
+)
+router.register(
+    "prevcad/text_recommendations/<int:pk>/register_click",
+    TextRecomendationsView,
+    basename="recommendation-register-click",
+)
 
-router.register('prevcad/appointments/', AppointmentViewSet, basename='appointments')
+router.register("prevcad/appointments/", AppointmentViewSet, basename="appointments")
 # Rutas de la API
 urlpatterns = [
-  path('prevcad/', include(router.urls)),
-  path('', include(router.urls)),
-  path("api_auth/", include("rest_framework.urls", namespace="rest_framework")),
-  path('prevcad/user/profile/', getProfile, name='get_profile'),
-  path('prevcad/user/profile/upload_image/', uploadProfileImage, name='upload_profile_image'),
-  path('prevcad/user/profile/delete_image/', deleteProfileImage, name='delete_profile_image'),
-  path('prevcad/health_categories/', HealthCategoryListView.as_view(), name='health-categories'),
-  path('prevcad/health-categories/<int:category_id>/responses/', save_evaluation_responses, name='responses'),
-  path('prevcad/health_categories/<int:category_id>/clear_evaluation/', 
-       health_categories.clear_evaluation, 
-       name='clear_evaluation'),
-  path('prevcad/text_recommendations/<int:pk>/register_click', TextRecomendationsView.as_view({'post': 'register_click'}), name='recommendation-register-click'),
-  path('prevcad/health-categories/create', 
-       create_health_category, 
-       name='create_health_category'),
-  path('prevcad/health-categories/<int:category_id>/update', 
-       update_health_category, 
-       name='update_health_category'),
-  path('admin/update-evaluation-form/<int:template_id>/', admin_views.update_evaluation_form, name='update_evaluation_form'),
-  path('admin/update-training-form/<int:template_id>/', admin_views.update_training_form, name='update_training_form'),
-  path('admin/healthcategory/<int:category_id>/update-recommendation/', 
-       update_recommendation, 
-       name='update_recommendation'),
- 
-  path('admin/healthcategory/<int:category_id>/save-professional-evaluation/',
-       save_professional_evaluation,
-       name='save_professional_evaluation'),
-  path('admin/categorytemplate/<int:template_id>/update_training_form/', 
-       update_training_form, 
-       name='update_training_form'),
-  path(
-    'admin/prevcad/categorytemplate/<int:template_id>/delete_training_node/<int:node_id>/',
-    admin_views.delete_training_node,
-    name='delete_training_node'
-  ),
-
+    path("prevcad/", include(router.urls)),
+    path("", include(router.urls)),
+    path("api_auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("prevcad/user/profile/", getProfile, name="get_profile"),
+    path(
+        "prevcad/user/profile/upload_image/",
+        uploadProfileImage,
+        name="upload_profile_image",
+    ),
+    path(
+        "prevcad/user/profile/delete_image/",
+        deleteProfileImage,
+        name="delete_profile_image",
+    ),
+    path(
+        "prevcad/health_categories/",
+        HealthCategoryListView.as_view(),
+        name="health-categories",
+    ),
+    path(
+        "prevcad/health-categories/<int:category_id>/responses/",
+        save_evaluation_responses,
+        name="responses",
+    ),
+    path(
+        "prevcad/health_categories/<int:category_id>/clear_evaluation/",
+        health_categories.clear_evaluation,
+        name="clear_evaluation",
+    ),
+    path(
+        "prevcad/text_recommendations/<int:pk>/register_click",
+        TextRecomendationsView.as_view({"post": "register_click"}),
+        name="recommendation-register-click",
+    ),
+    path(
+        "prevcad/health-categories/create",
+        create_health_category,
+        name="create_health_category",
+    ),
+    path(
+        "prevcad/health-categories/<int:category_id>/update",
+        update_health_category,
+        name="update_health_category",
+    ),
+    path(
+        "admin/update-evaluation-form/<int:template_id>/",
+        admin_views.update_evaluation_form,
+        name="update_evaluation_form",
+    ),
+    path(
+        "admin/update-training-form/<int:template_id>/",
+        admin_views.update_training_form,
+        name="update_training_form",
+    ),
+    path(
+        "admin/healthcategory/<int:category_id>/update-recommendation/",
+        update_recommendation,
+        name="update_recommendation",
+    ),
+    path(
+        "admin/healthcategory/<int:category_id>/save-professional-evaluation/",
+        save_professional_evaluation,
+        name="save_professional_evaluation",
+    ),
+    path(
+        "admin/categorytemplate/<int:template_id>/update_training_form/",
+        update_training_form,
+        name="update_training_form",
+    ),
+    path(
+        "admin/prevcad/categorytemplate/<int:template_id>/delete_training_node/<int:node_id>/",
+        admin_views.delete_training_node,
+        name="delete_training_node",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
