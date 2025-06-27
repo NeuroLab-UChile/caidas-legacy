b42e600156eca1b753156750ff0ecba414374bf8
Juano Venegas <jevenegasa@gmail.com>
2025-06-27 18:17:45 -0400

    Fixed appointments endpoint to include status

--- backend/prevcad/serializers/appointment_serializer.py
+++ backend/prevcad/serializers/appointment_serializer.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from ..models import Appointment
 
+
 class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appointment
-        fields = ['id', 'title', 'date', 'description', 'created_at']
-        read_only_fields = ['created_at'] 
+        # fields = ['id', 'title', 'date', 'description', 'created_at']
+        fields = "__all__"
+        read_only_fields = ["created_at"]
