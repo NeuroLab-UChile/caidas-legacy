e305983fb4ad67a81a61e8061a0751efdd6a13d0
Juano Venegas <jevenegasa@gmail.com>
2025-07-01 21:45:01 -0400

    Include DownloadableContent data in DownloadByUser endpoint

--- backend/prevcad/serializers/downloads_serializer.py
+++ backend/prevcad/serializers/downloads_serializer.py
@@ -4,7 +4,15 @@ from django.utils.encoding import smart_str
 from prevcad.models import DownloadableContent, DownloadByUser
 
 
+class DownloadableContentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DownloadableContent
+        fields = "__all__"
+
+
 class DownloadByUserSerializer(serializers.ModelSerializer):
+    content = DownloadableContentSerializer(read_only=True)
+
     class Meta:
         model = DownloadByUser
         fields = "__all__"
